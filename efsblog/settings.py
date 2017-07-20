ca035f368ac779cb891d8291882054ac77ff0346
mgorti <mgorti@unomaha.edu>
2017-07-20 12:17:24 -0500

    changes are made in settings.py

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
