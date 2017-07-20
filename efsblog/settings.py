d4bb6071ec71de6066594493f39249a4435375bc
mgorti <mgorti@unomaha.edu>
2017-07-20 12:33:49 -0500

    changes are made in settings file

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '=hj1a9+fe&)r*!wxdi6j$79ohr$xm2u7$8g@d(kn^#ep6lig*^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['efs-mgorti.herokuapp.com']
 
 
 # Application definition
